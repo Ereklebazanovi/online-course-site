9b8ce8e08cad3e7c621d4ca512af130f49e4e312
Erekle Bazanovi <erekle.bazanovi.1@iliauni.edu.ge>
2025-04-30 15:27:31 +0400

    fix: export Firestore and correct storageBucket in firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,16 +1,19 @@
 // src/firebase.ts
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAHDQ1kHPIlReqK2zgysjmQxPvnvIl1kcU",
   authDomain: "onlinecourses-2894c.firebaseapp.com",
   projectId: "onlinecourses-2894c",
-  storageBucket: "onlinecourses-2894c.firebasestorage.app",
+  storageBucket: "onlinecourses-2894c.appspot.com", // ← .appspot.com
   messagingSenderId: "546233525742",
   appId: "1:546233525742:web:e5bd2d7da60c5fd3679aff",
   measurementId: "G-D2EZKLXQLV",
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
+export const db = getFirestore(app);
