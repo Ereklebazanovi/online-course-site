7ff35999a7f9f6b494f80e0a7e64ad3c19ab4d74
Erekle Bazanovi <erekle.bazanovi.1@iliauni.edu.ge>
2025-05-17 20:20:41 +0400

    update with query

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -16,7 +16,7 @@ const FeaturedCourses = () => {
     <div className="p-6">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
         <h2 className="text-3xl font-bold text-gray-800 mb-8 text-center">
-          🌟 შემოთავაზებული კურსები
+           შემოთავაზებული კურსები
         </h2>
 
         {loading ? (
